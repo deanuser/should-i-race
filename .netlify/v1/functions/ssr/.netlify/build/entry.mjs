0693b2bb85d5b63d5ae7a68c29d68a3f4e36a46c
Dean <deandavis@Deans-MacBook-Pro-2.local>
2026-04-02 15:22:59 +0100

    Fix production CSS bundling with PostCSS

--- .netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -1,1 +1,1 @@
-export { q as createHandler } from './chunks/ssr-function_DFINGRnz.mjs';
+export { q as createHandler } from './chunks/ssr-function_BZmvfG1x.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
   adapter: netlify()
 });
